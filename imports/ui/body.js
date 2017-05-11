ff6861a1b4c023b2794be870e6c2f2c6ec6084eb
Shuhei Nomura <info@danmaq.com>
2017-05-11 19:59:39 +0900

    Tutorial 4: Added form and auto insert.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,5 +5,24 @@ import { Tasks } from '../api/tasks.js';
 import './body.html';
 
 Template.body.helpers({
-    tasks() { return Tasks.find({}); },
+    tasks() {
+        return Tasks.find({}, { sort: { createdAt: -1 } });
+    },
+});
+
+Template.body.events({
+    'submit .new-task' (event) {
+        // stop propagation
+        event.preventDefault();
+
+        // get value.
+        const target = event.target;
+        const text = target.text.value;
+
+        // insert to DB.
+        Tasks.insert({ text, createdAt: new Date() });
+
+        // clear form.
+        target.text.value = '';
+    },
 });
