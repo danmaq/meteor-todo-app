aaf67e32301235d69aad51bc0f0e6204a3931a60
Shuhei Nomura <info@danmaq.com>
2017-05-11 10:26:49 +0900

    Step3: Show collection using mongoDB backend.

--- client/main.js
+++ client/main.js
@@ -1,23 +1,3 @@
 'use strict';
 
 import '../imports/ui/body.js';
-/*
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
-
-Template.hello.onCreated(
-    // counter starts at 0
-    () => Template.instance().counter = new ReactiveVar(0));
-Template.hello.helpers({
-    counter() { return Template.instance().counter.get(); },
-});
-
-Template.hello.events({
-    'click button' (event, instance) {
-        const cnt = instance.counter;
-        cnt.set(cnt.get() + 1);
-    },
-});
-*/
